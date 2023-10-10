12e6b59566f3bec220fed6579f31bb9e34408e4d
Horaci Mota <horaciomota93@gmail.com>
2023-10-10 20:33:33 +0100

    Tech bullets added

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -39,7 +39,7 @@ function Hero() {
 
         <ul
           className="flex space-x-4"
-          style={{ fontSize: "28px", marginTop: "58px" }}
+          style={{ fontSize: "28px", marginTop: "28px", marginBottom: "28px" }}
         >
           <li>
             <a href="#" className="text-blue-600 hover:text-blue-700">
@@ -59,34 +59,74 @@ function Hero() {
         </ul>
       </div>
 
-      <div className="mx-auto max-w-screen-lg p-4 ">
-        <div className="flex flex-row items-center">
-          <div className="mr-4">
-            <img src={appicon} alt="Ícone do App" className="h-100" />
+      <div style={{ backgroundColor: "#f5f5f5" }}>
+        <div className="mx-auto max-w-screen-lg p-4 ">
+          <div className="flex flex-row items-center flex-wrap">
+            <div className="mr-4">
+              <img src={appicon} alt="Ícone do App" className="h-20" />
+            </div>
+            <div className="flex-grow">
+              <h2 className="mb-1" style={{ fontSize: "22px" }}>
+                <strong>Therapixie IOS 16+</strong>
+              </h2>
+              <p className="text-gray-600" style={{ fontSize: "16px" }}>
+                Therapixie simplifies therapy. I built using{" "}
+                <strong>SwiftUI</strong>, <strong>MVVM patterns</strong>,{" "}
+                <strong>SwiftData</strong>, and <strong>UITests</strong>
+              </p>
+            </div>
+            <div className="ml-auto hidden md:block">
+              <a
+                href="URL_DO_SEU_REPOSITORIO_NO_GITHUB"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn-apple"
+                style={{ padding: "10px 20px" }}
+              >
+                Check on GitHub
+              </a>
+            </div>
+
+            <div className="flex mt-4 flex-wrap hidden sm:block">
+              <div className="flex mt-2 flex-wrap">
+                <div className="bg-blue-100 text-gray-500 px-3 py-1 mr-2 rounded-full text-sm">
+                  SwiftUI
+                </div>
+                <div className="bg-blue-100 text-gray-500 px-3 py-1 mr-2 rounded-full text-sm">
+                  CoreData
+                </div>
+                <div className="bg-blue-100 text-gray-500 px-3 py-1 mr-2 rounded-full text-sm">
+                  MVVM
+                </div>
+                <div className="bg-blue-100 text-gray-500 px-3 py-1 mr-2 rounded-full text-sm">
+                  Clean Code
+                </div>
+                <div className="bg-blue-100 text-gray-500 px-3 py-1 mr-2 rounded-full text-sm">
+                  Swift Package Manager
+                </div>
+                <div className="bg-blue-100 text-gray-500 px-3 py-1 mr-2 rounded-full text-sm">
+                  Combine Framework
+                </div>
+                <div className="bg-blue-100 text-gray-500 px-3 py-1 mr-2 rounded-full text-sm">
+                  XCTest
+                </div>
+              </div>
+            </div>
           </div>
-          <div>
-            <h2 className="mb-1" style={{ fontSize: "28px" }}>
-              <strong>Therapixie IOS 16+</strong>
-            </h2>
-            <p className="text-gray-600" style={{ fontSize: "16px" }}>
-              Therapixie simplifies therapy. I built using{" "}
-              <strong>SwiftUI</strong>, <strong>MVVM patterns</strong>,{" "}
-              <strong>SwiftData</strong> and <strong>UITests</strong>
-            </p>
-          </div>
-          <div className="ml-auto">
+
+          <img src={iPhoneMockup} alt="Descrição da Imagem" className="" />
+
+          <div className="md:hidden flex justify-center items-center h-full">
             <a
               href="URL_DO_SEU_REPOSITORIO_NO_GITHUB"
               target="_blank"
               rel="noopener noreferrer"
-              className="btn-apple"
+              className="btn-apple md:w-full "
             >
-              Ver no GitHub
+              Check on GitHub (Mobile)
             </a>
           </div>
         </div>
-
-        <img src={iPhoneMockup} alt="Descrição da Imagem" className="" />
       </div>
     </>
   );
